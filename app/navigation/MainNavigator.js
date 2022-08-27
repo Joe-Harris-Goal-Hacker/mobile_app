4039a33d8b19617b6a9933203cd5368434592c72
Kevin-Zhouu <bigsoupzhou@gmail.com>
2022-08-27 11:09:37 +1000

    navigation

--- app/navigation/MainNavigator.js
+++ app/navigation/MainNavigator.js
@@ -1,8 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-
-import { Button, View, Text } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
+import { StyleSheet, Text, View, NavigationContainer } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Login } from "../screens/Login";
 const Stack = createNativeStackNavigator();
