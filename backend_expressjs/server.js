65caad3f544a3b71880af934a8b5179eee35186a
蔡森楠 <shauncai@006deMacBook-Pro.local>
2022-08-28 16:45:36 +1000

    added author

--- backend_expressjs/server.js
+++ backend_expressjs/server.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-
+//author:shaun
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
 });
