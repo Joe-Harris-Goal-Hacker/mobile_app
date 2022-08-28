5759ddc8d4e76e467566a87b8efcc145d93ac3fe
Kevin-Zhouu <bigsoupzhou@gmail.com>
2022-08-28 14:32:43 +1000

    fixed post

--- app/screens/Agree.js
+++ app/screens/Agree.js
@@ -2,20 +2,20 @@ import react, { useEffect, useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { Header } from "../components/Header";
 
-const Agree = ({title, imgsource}) => {
+const Agree = ({}) => {
   return (
     <>
-    <View style={{flexDirection: 'column'}}>
-      <Header title={title}/>
-      <Image source={{uri: imgsource}}/>
-    </View>
-    <View style={{flexDirection: 'row'}}>
-      <Button title = "Agree"/>
-      <Button title = "Neutral"/>
-      <Button title = "Disagree"/>
-    </View>
+      <View style={{ flexDirection: "column" }}>
+        <Header title={title} />
+        <Image source={{ uri: imgsource }} />
+      </View>
+      <View style={{ flexDirection: "row" }}>
+        <Button title="Agree" />
+        <Button title="Neutral" />
+        <Button title="Disagree" />
+      </View>
     </>
-  )
+  );
 };
 export { Agree };
 
@@ -23,10 +23,9 @@ const styles = StyleSheet.create({
   img: {
     width: 200,
     height: 200,
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
-
 
 // { navigation, route, ...props }
